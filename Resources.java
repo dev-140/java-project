46dceaed099d0e0db7955910d58979b795468a3f
Rox <roxmarzan14@gmail.com>
2023-02-22 19:07:02 +0800

    Create Resources.java

---
+++ Resources.java
@@ -0,0 +1,36 @@
+package main;
+
+public class Resources {
+
+//	ERROR MESSAGE
+	public static String errorMessageOne = "Please place a new order.*",
+			errorMessageTwo = "Enter only numbers not letters.*",
+			errorMessageThree = "Insufficient amount.*",
+			errorMessageFour = "Please place an order.*";
+	
+//	TEXT BUTTON
+	public static String cancel = "Cancel",
+				cheesecake = "Cheesecake",
+				coffee = "Coffee",
+				done = "Done",
+				juice = "Juice",
+				large = "Large",
+				medium = "Medium",
+				small = "Small",
+				spongecake = "Spongecake";
+				
+//	LABELS
+	public static String cakes = "Cakes",
+				change = "Change:",
+				drinks = "Drinks",
+				emptyNo = "0000.0",
+				enterAmount = "Enter Amount",
+				enterQty = "Enter Quantity",
+				newOrder = "New Order",
+				order = "Order",
+				selectSize = "SelectSize",
+				total = "Total:",
+				pay = "Pay";
+	
+	public static String convertToString = "";
+}
